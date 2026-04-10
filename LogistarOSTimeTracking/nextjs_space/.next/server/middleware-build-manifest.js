a9ec07f167c17a92e8963ec407f4cae182a72eb1
Jay <>
2026-04-10 11:46:54 -0400

    init with i18n and 4-step clock logic

---
+++ LogistarOSTimeTracking/nextjs_space/.next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack-e8a85626.js",
+    "static/chunks/main-app-0188775c.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
